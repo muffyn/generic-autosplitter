e6a567fd0ee80c2f18fe30fa8e850fa6688f70d4
Muffyn <kappatv@hotmail.com>
2025-07-04 12:51:32 -0400

    Fix configgroup

--- src/main/java/com/genericautosplitter/GenericAutosplitterConfig.java
+++ src/main/java/com/genericautosplitter/GenericAutosplitterConfig.java
@@ -5,7 +5,7 @@ import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
 import net.runelite.client.config.ConfigSection;
 
-@ConfigGroup("example")
+@ConfigGroup("autosplitter")
 public interface GenericAutosplitterConfig extends Config
 {
 	@ConfigItem(
